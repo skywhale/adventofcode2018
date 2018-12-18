021ba05dcb9805ca2c136bd784493fee24a8f7d0
Ryo Kawaguchi <ryo@kawagu.ch>
2018-12-18 23:37:08 +0900

    Add solution for day2.

---
+++ day2-part1/src/main.rs
@@ -0,0 +1,49 @@
+use std::fs;
+
+fn solver(input: &str) -> i32 {
+    let mut count_two = 0;
+    let mut count_three = 0;
+    for id in input.split(char::is_whitespace) {
+        // A counter for each ASCII code between 'a' and 'z'.
+        let mut counter = vec![0; 26];
+        for c in id.chars() {
+            let index = ((c as u8) - ('a' as u8)) as usize;
+            counter[index] += 1;
+        }
+        let mut found_two = false;
+        let mut found_three = false;
+        for count in counter {
+            if count == 2 && !found_two {
+                found_two = true;
+                count_two += 1;
+            }
+            if count == 3 && !found_three {
+                found_three = true;
+                count_three += 1;
+            }
+        }
+    }
+    count_two * count_three
+}
+
+fn main() {
+    let input = fs::read_to_string("/tmp/day2-input.txt").expect("Input file missing");
+    let result = solver(&input);
+    println!("{}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solver("abcdef"), 0);
+        assert_eq!(solver("bababc"), 1);
+        assert_eq!(solver("abbcde"), 0);
+        assert_eq!(
+            solver("abcdef bababc abbcde abcccd aabcdd abcdee ababab"),
+            12
+        );
+    }
+}
